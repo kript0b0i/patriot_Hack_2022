c32f925d880569f0ddca3abd7eb5980ba8645b75
kript0b0i <36682116+kript0b0i@users.noreply.github.com>
2022-03-13 11:01:56 -0400

    Update get_info.py

--- get_info.py
+++ get_info.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 import time
 from datetime import date
 import pytextnow as pytn
+from decouple import config
 
 def getSemester():
 
@@ -27,24 +30,25 @@ def getSemester():
     elif((month == '08' and day > 15) or month == '09' or month == '10' or month == '11' or month == '12'):
         season = 'Fall'
 
-    semester = season + ' ' + year
-    print(semester)
-
+    semester = season + ' 20' + year
     return semester
 
 
-def bot(netID, Password, Commands): 
-    s=Service('/usr/bin/chromedriver')
+def bot(netID, Password, Commands, UserNum): 
+    #netID = config('USERNAME')
+    #Password = config('PASSWORD')
+    s=Service('/Users/dannychung/StudentReminder/chromedriver')
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--incognito")
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
     Bot_Chrome = webdriver.Chrome(service=s, options=options )  
+
     print("running script..")
-    client = pytn.Client("dannychung333", sid_cookie="s%3AUzbX3dzpU2DDWhXrpnOayIeKncK0JjhN.R20zdRsZWF2tCDIS5VzjTtIDL4UqDG15qomj8%2FARM1Q", csrf_cookie="s%3A1-RmzV2mdnfXy-MFNLMpnQaT.7hPEJC5lU67MAcewwrlET%2BHSpx33uUMB4GqRIW7vNIU")
+    client = pytn.Client("luis29798", sid_cookie="s%3A8fhrkEnwxcHwt51RtxbgBYIOeH_bhxAS.OmVatrUGxqWWZ80coYN0VhJ2G0K15MXH5ggQUQwY%2FnM", csrf_cookie="s%3A0wmGXLUFhATOQU875LeVbnal.m7qZODeJLASfzBqdalYjMIiAccFGlZSPkLyCelL88kA")
     print("opening website")
     Bot_Chrome.get("https://mymasonportal.gmu.edu/")
     time.sleep(1)
@@ -59,19 +63,57 @@ def bot(netID, Password, Commands):
     Bot_Chrome.find_element(By.XPATH, '/html/body/div/div/div/div[2]/form/div[5]/button').click()
     title = Bot_Chrome.title
     if title != "George Mason Federated Login Service":
-        client.send_sms("5714842469", "Success Login")
+        client.send_sms(UserNum, "Success Login")
         title = Bot_Chrome.title
         if title == "Information Release":
+            #clicks accept button for information release
             Bot_Chrome.find_element(By.XPATH, '/html/body/form/div/div[2]/p[2]/input[2]').click()
-            time.sleep(1)
-            Bot_Chrome.find_element(By.XPATH, '//*[@id="base_tools"]/bb-base-navigation-button[4]/div/li/a').click()
-            time.sleep(1)
-            Bot_Chrome.find_element(By.XPATH,'//*[@id="courses-filter"]/li[3]/a/span').click()
-            time.sleep(1)
-            Bot_Chrome.find_element(By.XPATH, '//*[@id="main-content-inner"]/div/div[1]/div[1]/div/div/div[3]/div/header/bb-search-box/div/input').send_keys("")
+            time.sleep(2)
+            WebDriverWait(Bot_Chrome,10000).until(EC.visibility_of_element_located((By.TAG_NAME,'body')))
+            #click courses navigation button
+            Bot_Chrome.find_element(By.LINK_TEXT, 'Courses').click()
+            #click grid toggle
+            time.sleep(2)
+            Bot_Chrome.find_element(By.CSS_SELECTOR, 'label.toggle-label.input.label-two.js-label-toggle-grid').click()
+            time.sleep(2)
+            #WebDriverWait(Bot_Chrome, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-content-inner"]/div/div[1]/div[1]/div/div/div[2]/div/header/bb-search-box'))).click()
+            Bot_Chrome.find_element_by_css_selector('bb-search-box>div>input').send_keys('Fall 2021')    
 
+            time.sleep(2)
+            parentCourseDiv = Bot_Chrome.find_element_by_class_name('course-org-list')
+            childElements = parentCourseDiv.get_attribute("childElementCount")
+            childElementsNum = int(childElements) - 1
+
+            todos = []
+            i = 1
+            
+            all_course_cards = Bot_Chrome.find_elements_by_tag_name('bb-base-course-card')
+            all_course_cards.pop()
+            for course in all_course_cards:
+                course.click()
+                time.sleep(3)
+
+                iframe = Bot_Chrome.find_element_by_xpath("//iframe[@name='classic-learn-iframe']")
+                print(iframe)
+                Bot_Chrome.switch_to.frame(iframe)
+              
+                div = Bot_Chrome.find_elements(By.CSS_SELECTOR, '#blocklist\:\:3-dueView\:\:\:\:\:3-dueView_3')
+                
+                for todo in div:
+                    print(todo.text)
+                
+                time.sleep(3)
+                Bot_Chrome.find_element_by_class_name('bb-close').click()
+            
+            print(childElementsNum)
+            if(childElementsNum == -1):
+                client.send_sms(UserNum,'no classes for current semester')
+            #print(todos)
+            time.sleep(45)
 
     else:
-        client.send_sms("5714842469", "Fail login")
+        client.send_sms(UserNum, "Fail login")
     Bot_Chrome.close()
+
+    
 
